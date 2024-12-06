0d622cf180c5e1b462d7c9c0854a6f851cce2596
Marcio Nicolau <marcio.nicolau@inf.ufrgs.br>
2024-12-06 13:05:00 +0000

    feat: add lint-staged

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,16 +4,16 @@ echo -e "\nStarting post create command script..."
 echo "Dev machine:"
 uname -a
 
- git config --global core.autocrlf input
+git config --global core.autocrlf input
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 omz update
 omz plugin load gh npm git-prompt themes python node github pre-commit
 omz plugin enable gh npm git-prompt themes python node github pre-commit
 
-git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula"
-ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
+# git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula"
+# ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 omz theme set spaceship
@@ -21,13 +21,15 @@ omz theme set spaceship
 mkdir -p ~/.config
 cp .devcontainer/spaceship.zsh ~/.config/spaceship.zsh
 
-bash -c "$(echo "N" | curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+bash -c "$(echo "n" | curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 omz reload 
 
 zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
 zinit light zsh-users/zsh-completions
+
+omz reload
 
 echo -e "\n*******************************"
 echo -e "\nDev container ready!".
