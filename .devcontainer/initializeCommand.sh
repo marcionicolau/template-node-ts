9200e45a2390f9c16d7029711eef3e39501a6b4a
Marcio Nicolau <marcio.nicolau@inf.ufrgs.br>
2024-12-09 09:23:59 -0300

    feat: update to new configurations

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -2,10 +2,10 @@
 echo "Gathering you ip for dev container"
 
 ##############################################################################################
-# en (Ethernet) - ib (InfiniBand) - sl (Serial line IP (slip)) - wl (Wireless local area 
+# en (Ethernet) - ib (InfiniBand) - sl (Serial line IP (slip)) - wl (Wireless local area
 # network (WLAN)) - ww (Wireless wide area network (WWAN))
 #############################################################################################
-your_interface_name="eno" 
+your_interface_name="eno"
 interface_prefix="en" # Choose the interface network.
 iname=$(ip -o link show | sed -rn '/^[0-9]+: en/{s/.: ([^:]*):.*/\1/p}')
 ip=`ifconfig $iname  | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | sed 's/inet //g'`
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,26 +4,37 @@ echo -e "\nStarting post create command script..."
 echo "Dev machine:"
 uname -a
 
+git config --global user.email "marcio.nicolau@inf.ufrgs.br"
+git config --global user.name "Marcio Nicolau"
 git config --global core.autocrlf input
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 omz update
-omz plugin load gh npm git-prompt themes python node github pre-commit
-omz plugin enable gh npm git-prompt themes python node github pre-commit
 
-# git clone https://github.com/dracula/zsh.git "$ZSH_CUSTOM/themes/dracula"
-# ln -s "$ZSH_CUSTOM/themes/dracula/dracula.zsh-theme" "$ZSH_CUSTOM/themes/dracula.zsh-theme"
+ZSH_CUSTOM="$ZSH/custom"
+echo $ZSH
+echo $ZSH_CUSTOM
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-omz theme set spaceship
+
+omz plugin load gh npm git-prompt themes python node github pre-commit
+omz plugin enable gh npm git-prompt themes python node github pre-commit
+
+# Install biomejs
+mkdir -p "$HOME/.local/bin"
+curl -L https://github.com/biomejs/biome/releases/download/cli%2Fv1.9.4/biome-linux-x64 -o "$HOME/.local/bin/biome"
+chmod +x "$HOME/.local/bin/biome"
 
 mkdir -p ~/.config
 cp .devcontainer/spaceship.zsh ~/.config/spaceship.zsh
 
-bash -c "$(echo "n" | curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+omz theme set spaceship
 
-omz reload 
+bash -c "$(echo "Y" | curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+omz reload
 
 zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
